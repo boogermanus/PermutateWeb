71e0509be37944740113b20ac9a4bc384a39ac02
Jordan Woodruff <boogermanus@gmail.com>
2025-01-31 11:14:40 -0600

    add home controller

--- PermauteWebApi/Program.cs
+++ PermauteWebApi/Program.cs
@@ -1,9 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://localhost:5007");
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -15,5 +17,7 @@ if (app.Environment.IsDevelopment())
 }
 
 // app.UseHttpsRedirection();
+// app.UseRouting();
 
+app.MapControllers();
 app.Run();
