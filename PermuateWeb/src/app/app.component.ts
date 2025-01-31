9b9a4a9e8a874c73d676962f11d77f49812fc6a0
Jordan Woodruff <boogermanus@gmail.com>
2025-01-31 13:54:25 -0600

    add bootstrap import

--- PermuateWeb/src/app/app.component.ts
+++ PermuateWeb/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileComponent } from "./components/profile/profile.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
