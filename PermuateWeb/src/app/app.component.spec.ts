c289538e81407631355f9f4fa10c943897a7d3d8
Jordan Woodruff <boogermanus@gmail.com>
2025-01-31 13:44:21 -0600

    remove base code

--- PermuateWeb/src/app/app.component.spec.ts
+++ PermuateWeb/src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('PermuateWeb');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, PermuateWeb');
-  });
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('h1')?.textContent).toContain('Hello, PermuateWeb');
+  // });
 });
