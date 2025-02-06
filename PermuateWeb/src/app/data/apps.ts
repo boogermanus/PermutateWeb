0d8cf33f60e8bce724a42911e52e6b5a7e5f1933
Jordan Woodruff <boogermanus@gmail.com>
2025-02-06 11:26:48 -0600

    formatting for component

--- PermuateWeb/src/app/data/apps.ts
+++ PermuateWeb/src/app/data/apps.ts
@@ -4,9 +4,9 @@ const apps: IApp[] = [
     {
         id: 1,
         name: 'Tickers',
-        description: 'Tickers is a simple application that counts up or down in ticks (seconds). It was my first Angular application. The current application has been refactored to use Angular 18.',
-        appUrl: '',
-        gitHubUrl: ''
+        description: 'Tickers is a simple application that counts up or down in ticks (seconds). It was my first Angular application. The current version has been refactored to use Angular 18.',
+        appUrl: 'https://permutate.us/stuff/tickers',
+        gitHubUrl: 'https://github.com/boogermanus/tickers'
     }
 ]
 
