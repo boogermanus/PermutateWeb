88db0b1441df8b6d16f32016d3f0cae893337268
Jordan Woodruff <boogermanus@gmail.com>
2025-02-06 16:54:43 -0600

    Hotreload (#1)
    * this is stupid because it doesn't work
    
    * hotreload maybe working

--- PermutateWeb/src/app/components/app-list/app-list.component.ts
+++ PermutateWeb/src/app/components/app-list/app-list.component.ts
@@ -1,10 +1,12 @@
-import { AfterViewInit, ChangeDetectorRef, Component, OnInit, viewChild } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, ChangeDetectorRef, Component, OnChanges, OnInit, viewChild } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatAccordion, MatExpansionModule} from '@angular/material/expansion';
 import { AppService } from '../../services/app.service';
 import { IApp } from '../../interfaces/iapp';
 import { AppComponent } from '../app/app.component';
 import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-app-list',
@@ -13,37 +15,29 @@ import { MatButtonModule } from '@angular/material/button';
     MatCardModule,
     MatExpansionModule,
     AppComponent,
-    MatButtonModule
+    MatButtonModule,
+    CommonModule
   ],
   templateUrl: './app-list.component.html',
   styleUrl: './app-list.component.scss'
 })
-export class AppListComponent implements AfterViewInit {
-  public appList: IApp[] = [];
+export class AppListComponent implements OnInit {
+  public appList!: Observable<IApp[]>
   public accordion = viewChild.required(MatAccordion);
-  public expanded: boolean = false;
+  public expanded: boolean = true;
   public buttonText: string = 'Collapse All';
 
   constructor(
-    private readonly service: AppService,
-    private readonly crd: ChangeDetectorRef
+    private readonly service: AppService
   ) {
-    this.appList = this.service.getApps();
+
   }
 
-  ngAfterViewInit(): void {
-    this.toggle();
-    this.crd.detectChanges();
+  ngOnInit(): void {
+    this.appList = this.service.getAppsAsync();
   }
 
   public toggle(): void {
-    if(this.expanded) {
-      this.accordion().closeAll();
-    }
-    else {
-      this.accordion().openAll();
-    }
-    
     this.expanded = !this.expanded;
     this.buttonText = this.expanded ? 'Collapse All' : 'Expand All'
   }
