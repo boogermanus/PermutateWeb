0bef810ab30c96eec46cc43d553f670c03ea52fb
Jordan Woodruff <boogermanus@gmail.com>
2025-02-06 10:59:55 -0600

    add basis for expansion

--- PermuateWeb/src/app/components/app-list/app-list.component.ts
+++ PermuateWeb/src/app/components/app-list/app-list.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule} from '@angular/material/expansion';
 
 @Component({
   selector: 'app-app-list',
   standalone: true,
   imports: [
-    MatCardModule
+    MatCardModule,
+    MatExpansionModule
   ],
   templateUrl: './app-list.component.html',
   styleUrl: './app-list.component.scss'
