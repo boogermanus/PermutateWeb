56cf597f4d38a81b90f484c1e47c12677efb43ce
Jordan Woodruff <boogermanus@gmail.com>
2025-02-06 12:05:47 -0600

    expand/collapse

--- PermuateWeb/src/app/components/app-list/app-list.component.ts
+++ PermuateWeb/src/app/components/app-list/app-list.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, viewChild } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { MatExpansionModule} from '@angular/material/expansion';
+import { MatAccordion, MatExpansionModule} from '@angular/material/expansion';
 import { AppService } from '../../services/app.service';
 import { IApp } from '../../interfaces/iapp';
 import { AppComponent } from '../app/app.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-app-list',
@@ -11,15 +12,34 @@ import { AppComponent } from '../app/app.component';
   imports: [
     MatCardModule,
     MatExpansionModule,
-    AppComponent
+    AppComponent,
+    MatButtonModule
   ],
   templateUrl: './app-list.component.html',
   styleUrl: './app-list.component.scss'
 })
-export class AppListComponent {
+export class AppListComponent implements AfterViewInit {
   public appList: IApp[] = [];
-
+  public accordion = viewChild.required(MatAccordion);
+  public expanded: boolean = false;
+  public buttonText?: string;
   constructor(private readonly service: AppService) {
     this.appList = this.service.getApps();
+  }
+
+  ngAfterViewInit(): void {
+    this.toggle();
+  }
+
+  public toggle(): void {
+    if(this.expanded) {
+      this.accordion().closeAll();
+    }
+    else {
+      this.accordion().openAll();
+    }
+    
+    this.expanded = !this.expanded;
+    this.buttonText = this.expanded ? 'Collapse All' : 'Expand All'
   }
 }
