0d72650f325f48ae1e16d90ac86ae0034b41e22a
Jordan Woodruff <boogermanus@gmail.com>
2025-05-27 21:49:13 -0500

    link buttons

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component, Input } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { IApp } from '../../interfaces/iapp';
 import { TagsComponent } from "../tags/tags.component";
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-app',
   standalone: true,
   imports: [
     MatExpansionModule,
+    MatButtonModule,
     TagsComponent
 ],
   templateUrl: './app.component.html',
